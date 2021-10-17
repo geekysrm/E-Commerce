58109f9858cff1657c25d1c59fd794b9a17ab233
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2021-10-17 21:34:43 +0530

    Convert photoslide component to functional component

--- src/components/App/photoSlide.js
+++ src/components/App/photoSlide.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import { Carousel } from 'antd';
 import 'antd/dist/antd.css';
-import '../../style/photoSlide.css'
+import '../../style/photoSlide.css';
 
-class PhotoSlide extends React.Component
-{
-  render()
-  {
-    return (
-      <div style={{paddingTop:50}}>
-      <Carousel autoplay>
-        <div><img style={styles.photo} src="./assets/PhotoSlideRado.jpg" alt="rado" /></div>
-        <div><img style={styles.photo} src="./assets/PhotoSlideTissot.jpg" alt="tissot" /></div>
-        <div><img style={styles.photo} src="./assets/PhotoSlideTissot1.jpg" alt="tissot" /></div>
-        <div><img style={styles.photo} src="./assets/PhotoSlideFossil.gif" alt="fossil" /></div>
-      </Carousel>
-      </div>
-    );
-  }
-}
-
-const styles={
-  photo:{
-    height: 450 , width: "100%"
-  }
+const styles = {
+  photo: {
+    height: 450,
+    width: '100%',
+  },
 };
 
-export default PhotoSlide;
+export default function Navbar() {
+  return (
+    <div>
+      <div style={{ paddingTop: 50 }}>
+        <Carousel autoplay>
+          <div>
+            <img
+              style={styles.photo}
+              src="./assets/PhotoSlideRado.jpg"
+              alt="rado"
+            />
+          </div>
+          <div>
+            <img
+              style={styles.photo}
+              src="./assets/PhotoSlideTissot.jpg"
+              alt="tissot"
+            />
+          </div>
+          <div>
+            <img
+              style={styles.photo}
+              src="./assets/PhotoSlideTissot1.jpg"
+              alt="tissot"
+            />
+          </div>
+          <div>
+            <img
+              style={styles.photo}
+              src="./assets/PhotoSlideFossil.gif"
+              alt="fossil"
+            />
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  )
+}
+
+
+
